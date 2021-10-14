2c52dea06ca0202aa12df37c431656ba077747f3
Anna <apoletshuk@mail.ru>
2021-10-14 16:45:46 +0300

    added task1, task2, task3

---
+++ script_1.js
@@ -0,0 +1,55 @@
+//task2
+
+let a1 = 5 % 3;
+console.log(a1, typeof a1);
+
+let a2 = 3 % 5;
+console.log(a2, typeof a2);
+
+let a3 = 5 + '3';
+console.log(a3, typeof a3);
+
+let a4 = '5' - 3;
+console.log(a4, typeof a4);
+
+let a5 = 75 + 'кг';
+console.log(a5, typeof a5);
+
+let a6 = 785 * 653;
+console.log(a6, typeof a6);
+
+let a7 = 100 / 25;
+console.log(a7, typeof a7);
+
+let a8 = 0 * 0;
+console.log(a8, typeof a8);
+
+let a9 = 0 / 2;
+console.log(a9, typeof a9);
+
+let a10 = 89 / 0;
+console.log(a10, typeof a10);
+
+let a11 = 98 + 2;
+console.log(a11, typeof a11);
+
+let a12 = 5 - 98;
+console.log(a12, typeof a12);
+
+let a13 = (8 + 56 * 4) / 5;
+console.log(a13, typeof a13);
+
+let a14 = (9 -12) * 7 / (5 + 2);
+console.log(a14, typeof a14);
+
+let a15 = +'123';
+console.log(a15, typeof a15);
+
+let a16 = 1 || 0;
+console.log(a16, typeof a16);
+
+let a17 = false || true;
+console.log(a17, typeof a17);
+
+let a18 = true > 0;
+console.log(a18, typeof a18);
